f8e3a7da0edc2d48f8d2492b25f21e1d6d303dd7
AugustineChristo <69501547+AugustineChristo@users.noreply.github.com>
2020-12-06 16:47:30 +0400

    Update SampleForm.cs

--- KU.Repositories/Models/SampleForm.cs
+++ KU.Repositories/Models/SampleForm.cs
@@ -7,6 +7,7 @@ namespace KU.Repositories.Models
 {
     public class SampleForm
     {
+        public string LOCATION { get; set; }
         public string SAMPLINGDATE { get; set; }
         public string KUREFERENCE { get; set; }
         public string STATIONCODE { get; set; }
