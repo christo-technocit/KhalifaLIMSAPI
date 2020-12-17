c6f01a0517aded7c9d16ef3bde02cf63b2590047
AugustineChristo <69501547+AugustineChristo@users.noreply.github.com>
2020-12-17 12:07:28 +0400

    sampleform model update

--- KU.Repositories/Models/SampleForm.cs
+++ KU.Repositories/Models/SampleForm.cs
@@ -14,5 +14,25 @@ namespace KU.Repositories.Models
         public string INFLUENTFLOWRATE { get; set; }
         public string USERNAME { get; set; }
         public string COMPANYNAME { get; set; }
+
+        public string SAMPLETYPE { get; set; }
+        public string SAMPLESUBTYPE { get; set; }
+        public string SAMPLEVOLUME { get; set; }
+        public string SAMPLECOLLECTIONTYPE { get; set; }
+        public string STARTDATECOLLECTION { get; set; }
+        public string STARTTIMECOLLECTION { get; set; }
+        public string ENDDATECOLLECTION { get; set; }
+        public string ENDTIMECOLLECTION { get; set; }
+        public string COLLECTEDBY { get; set; }
+        public string EMIRATE { get; set; }
+        public string TEMPERATURE { get; set; }
+        public string  CONDUCTIVITY { get; set; }
+        public string DISSOLVEDOXYGEN { get; set; }
+        public string PH { get; set; }
+        public string POPULATIONESTIMATESERVEDBYSTATION { get; set; }
+        public string NUMBEROFINFECTEDPEOPLE { get; set; }
+        public string RNAVIRALCOUNTS { get; set; }
+        public string STANDARDDEVIATION { get; set; }
+
     }
 }
